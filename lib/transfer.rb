160aef96137537d9ec6c3bb35fd88a319e894768
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-19 01:35:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   # your code here
+  @processed = Array.new
 
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -18,9 +19,12 @@ class Transfer
       @status = "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
+      @processed << self
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer(transfer)
 end
