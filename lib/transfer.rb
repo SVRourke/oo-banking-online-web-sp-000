83dc69d9169dc491b1743739fe85d40ffbd3391c
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-19 01:35:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,10 @@ class Transfer
 
   def execute_transaction
     if @sender.valid? && @receiver.valid? && @sender.balance > @amount && @status == "pending"
+      @processed << self
       @status = "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @processed << self
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
